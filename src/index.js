2aa9e5a28a6893ae0ca4520da8279e4ecf53b54c
James Wotherspoon <james.t.wotherspoon@gmail.com>
2021-05-17 15:45:32 +0100

    the page has the basic structure and colours of each section

--- src/index.js
+++ src/index.js
@@ -2,15 +2,47 @@ import html from './index.html';
 import "./stylesheet.scss";
 import $ from 'jquery';
 
-$('aside').css('width', '100vw');
-setTimeout(foldAway, 2000);
 
-function foldAway(){
+$(function(){
+    $(function animatedGreeting(){
+
+        $('.message-part-two').hide();
+        $(function messagePartTwo(){
+            $('.message-part-two').fadeIn();
+            const msgPartTwo = `Hi, i'm a front-end web developer, based just outside london `;
+            const messageArrayPartTwo = msgPartTwo.split('');
+            let $partTwoCursor = $('.message-part-two .text-cursor');
+            $partTwoCursor.delay(2100).queue(function(){
+                $(this).append('|');
+            });
+            setInterval(() => {
+                $partTwoCursor.toggle();
+            }, 500);
+            let i = 0;
+            const printLettersPartTwo = setInterval(() => {
+                $('.message-part-two .message-letters').append(`${messageArrayPartTwo[i]}`);
+                i++;
+                if(i == messageArrayPartTwo.length ){ 
+                    clearInterval(printLettersPartTwo);
+                    setTimeout(() => {
+                        $partTwoCursor.empty();
+                    }, 3000);
+                };
+            }, 100);
+
+        })
+    });
     
-    $('aside').animate({width: '30vw'}, 500);
-}
+    $(function linkScrolling(){
+        $('a[href^="#"]').on('click', function(){
+            $('html').animate({
+                scrollTop: $( $.attr(this, 'href') ).offset().top
+            }, 500);
+            return false;
+        });
+    })
 
-
+})
 
 
   
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
   devServer: {
     contentBase: './dist',
   },
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
